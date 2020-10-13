9c564a84e39e0608982d99707db8cda75acfab7b
foolbread <981848153@qq.com>
2020-10-13 11:28:21 +0800

    +新增类型转换内存申请降低

---
+++ util/zeroalloc.go
@@ -0,0 +1,32 @@
+/*
+author: foolbread
+file: zeroalloc.go
+date: 2020/10/13 11:27 上午
+*/
+package util
+
+import "unsafe"
+
+func StringToByteSlice(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(&s))
+}
+
+func ByteSliceToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func Uint64ToInt64(val uint64) int64 {
+	return *(*int64)(unsafe.Pointer(&val))
+}
+
+func Uint64ToFloat64(val uint64) float64 {
+	return *(*float64)(unsafe.Pointer(&val))
+}
+
+func Int64ToUint64(val int64) uint64 {
+	return *(*uint64)(unsafe.Pointer(&val))
+}
+
+func Float64ToUint64(val float64) uint64 {
+	return *(*uint64)(unsafe.Pointer(&val))
+}
